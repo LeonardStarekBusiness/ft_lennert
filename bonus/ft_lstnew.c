628b33817790e7cacf407ad6bee724085b886263
unknown <leonard.starek.business@gmail.com>
2025-10-16 17:15:24 +0200

    Moloch Commit

--- bonus/ft_lstnew.c
+++ bonus/ft_lstnew.c
@@ -1,9 +1,11 @@
 t_list	*ft_lstnew(void *content)
 {
-	t_list	newl;
+	t_list	*newl;
 
 	newl = malloc(sizeof(t_list));
-	ft_memcpy(newl.content, content, sizeof(content));
-	newl.next = &newl + sizeof(content);
+	if (newl == NULL)
+		return (NULL);
+	(*newl).content = content;
+	(*newl).next = NULL;
 	return (newl);
 }
